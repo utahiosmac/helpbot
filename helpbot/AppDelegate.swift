9aa7a976c020ef962a8181ce37feb55ab15c9efa
Dave DeLong <davedelong@me.com>
2016-01-20 20:57:24 -0800

    Initial Commit

---
+++ helpbot/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  helpbot
+//
+//  Created by Dave DeLong on 1/20/16.
+//  Copyright © 2016 Utah iOS & Mac Developers. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
