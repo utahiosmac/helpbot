dc67055de41aeab0b2d91dbf746cac115047ea74
Dave DeLong <davedelong@me.com>
2016-10-05 09:54:27 -0600

    Updating to swift 3

--- helpbot/AppDelegate.swift
+++ helpbot/AppDelegate.swift
@@ -7,18 +7,22 @@
 //
 
 import Cocoa
+import botkit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    var bot: Bot?
 
 
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        bot = Bot(authorizationToken: "xoxb-3739968338-uZHXmix0jQm2jq2KOMHyFIEa")
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
+    func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
