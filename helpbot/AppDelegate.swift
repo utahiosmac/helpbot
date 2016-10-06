23ce5a9acdd67123be5dbb3aac462f9b5852aadb
Dave DeLong <davedelong@me.com>
2016-10-06 16:34:05 -0600

    Update to use new Bot API. Also, log the emoji when the bot connects

--- helpbot/AppDelegate.swift
+++ helpbot/AppDelegate.swift
@@ -23,12 +23,41 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             fatalError("Unable to find authToken.txt resource")
         }
         
+        guard let url = FileManager.default.urls(for: .cachesDirectory, in: [.userDomainMask]).first else {
+            fatalError("Unable to find caches directory")
+        }
+        
         do {
             let authToken = try String(contentsOf: authTokenFile)
             let trimmed = authToken.trimmingCharacters(in: .whitespacesAndNewlines)
-            bot = Bot(authorizationToken: trimmed)
+            
+            let configuration = Bot.Configuration(authToken: trimmed, dataDirectory: url)
+            bot = Bot(configuration: configuration)
         } catch let e {
             fatalError("Unable to read auth token from authToken.txt resource: \(e)")
+        }
+        
+        bot?.on { (e: Channel.Archived, b: Bot) in
+            print("Archived: \(e.channel)")
+        }
+        
+        bot?.on { (e: Channel.UserLeft, b: Bot) in
+            print("User left \(e.channel): \(e.user)")
+        }
+        
+        bot?.on { (e: Channel.UserJoined, b: Bot) in
+            print("User joined \(e.channel): \(e.user)")
+        }
+        
+        bot?.on { (e: Channel.MessagePosted, b: Bot) in
+            let s = "In \(e.message.channel), \(e.message.user) said: \(e.message.text)"
+            print(s)
+        }
+        
+        bot?.on { (e: Startup, b: Bot) in
+            b.execute(action: Emoji.List(), completion: { r in
+                print("result: \(r)")
+            })
         }
     }
 
